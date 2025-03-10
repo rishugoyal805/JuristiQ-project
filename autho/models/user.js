ffaafe800e9a95c699607a5d7c18936c11834613
devyani1512 <148792031+devyani1512@users.noreply.github.com>
2025-03-10 13:01:32 +0530

    Update user.js

--- autho/models/user.js
+++ autho/models/user.js
@@ -1,16 +1,18 @@
-        const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-        mongoose.connect("mongodb://127.0.0.1:27017/miniproject");
+mongoose.connect("mongodb://127.0.0.1:27017/miniproject");
 
-        const userSchema= mongoose.Schema({
-            name: String,
-            age: Number,
-            email: String,
-            password: String,
-            
-            otp: Number,
-            posts: [
-                { type: mongoose.Schema.Types.ObjectId, ref: "post"}
-            ]
-        });
-        module.exports= mongoose.model('user', userSchema);
+const userSchema = mongoose.Schema({
+  name: String,
+  age: Number,
+  email: String,
+  password: String,
+  contact: String, // Add contact field
+  casesHandled: Number, // Add casesHandled field
+  casesWon: Number, // Add casesWon field
+  profilePic: String, // Add profilePic field
+  otp: Number,
+  posts: [{ type: mongoose.Schema.Types.ObjectId, ref: "post" }]
+});
+
+module.exports = mongoose.model("user", userSchema);
