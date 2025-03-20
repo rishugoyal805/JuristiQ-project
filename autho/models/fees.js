57ba4fe4b11bb0d1563dc0418c5c5106d599e00e
devyani1512 <148792031+devyani1512@users.noreply.github.com>
2025-03-20 23:24:14 +0530

    Update fees.js

--- autho/models/fees.js
+++ autho/models/fees.js
@@ -1,15 +1,18 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017/miniproject");
-const FeesSchema = new mongoose.Schema({
-  case_ref_no: { type: Number, required: true, unique: true },
+
+const feesSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "user", required: true }, // User reference
+  case_ref_no: { type: String, required: true },
   clientName: { type: String, required: true },
   fees: { type: Number, required: true },
   amount_paid: { type: Number, required: true },
   pending_fees: { type: Number, required: true },
-  payment_mode: { type: String, required: true },
+  payment_mode: { type: String, enum: ["Cash", "Card", "Online"], required: true },
   due_date: { type: Date, required: true },
   remarks: { type: String },
 });
 
-const FeesModel = mongoose.model("Fees", FeesSchema);
-module.exports = FeesModel;
+
+
+module.exports= mongoose.model('fees', feesSchema);
+
