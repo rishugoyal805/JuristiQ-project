a444f743668e8ac87b3bb6a5729835e59c8a0d5b
devyani1512 <148792031+devyani1512@users.noreply.github.com>
2025-03-20 23:22:47 +0530

    Update client.js

--- autho/models/client.js
+++ autho/models/client.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose');
 
 
-mongoose.connect("mongodb://127.0.0.1:27017/miniproject");
+
 const clientSchema= mongoose.Schema({
+     user: { 
+            type: mongoose.Schema.Types.ObjectId, 
+            ref: "user", 
+            required: true // Ensures each case is associated with a user
+        },  
   client_name: {
     type: String,
     required: true, // Ensures the client name is mandatory
