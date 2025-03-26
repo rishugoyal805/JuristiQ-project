79622b20f8cfe165ecb84548e0c4f1cbc5e8965a
SwayamGupta12345 <Swayamsam2005@gmail.com>
2025-03-26 15:07:52 +0530

    just some edits

--- JuristiqReact/vite.config.js
+++ JuristiqReact/vite.config.js
@@ -5,5 +5,5 @@ export default defineConfig({
   plugins: [react()],
   server: {
     allowedHosts: ['positive-liberal-treefrog.ngrok-free.app','personally-allowing-lacewing.ngrok-free.app','one-famous-sculpin.ngrok-free.app'],
-  },
+},
 })
--- autho/models/user.js
+++ autho/models/user.js
@@ -1,22 +1,20 @@
 const mongoose = require("mongoose");
 // mongoose.connect("mongodb+srv://swayamsam2005:sLDNreRmb5R0KjQH@cluster0.ipxl289.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 require("../db"); // Import the connection file
-// mongoose.connect("mongodb+srv://swayamsam2005:sLDNreRmb5R0KjQH@cluster0.ipxl289.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
-//  use mongodb+srv://swayamsam2005:sLDNreRmb5R0KjQH@cluster0.ipxl289.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0 
 const userSchema = mongoose.Schema({
-  name: String,
-  age: Number,
-  email: String,
-  password: String,
-  contact: String, // Add contact field
-  casesHandled: Number, // Add casesHandled field
-  casesWon: Number, // Add casesWon field
-  profilePic: String, // Add profilePic field
-  otp: Number,
-  secretString: String,
-  cases: [{type: mongoose.Schema.Types.ObjectId, ref: "cases"}],
-  client: [{type: mongoose.Schema.Types.ObjectId, ref: "client"}],
-  fees: [{type: mongoose.Schema.Types.ObjectId, ref: "fees"}],
+  name: { type: String, required: true },
+  age: { type: Number, required: true },
+  email: { type: String, required: true, unique: true },
+  password:{type: String},
+  contact: { type: String, default: "" },
+  casesHandled: { type: Number, default: 0 },
+  casesWon: { type: Number, default: 0 },
+  profilePic: { type: String, default: "" },
+  otp: { type: Number, default: null },
+  secretString:{type: String},
+  cases: [{ type: mongoose.Schema.Types.ObjectId, ref: "cases" }],
+  client: [{ type: mongoose.Schema.Types.ObjectId, ref: "client" }],
+  fees: [{ type: mongoose.Schema.Types.ObjectId, ref: "fees" }],
 });
 
 module.exports = mongoose.model("user", userSchema);
