3032f6d027985973d6b68bde95238b8b544a0942
maanya.g14 <maanya.g14@gmail.com>
2025-04-11 21:54:31 +0530

    port

--- autho/app.js
+++ autho/app.js
@@ -670,6 +670,6 @@ app.delete("/deletefee/:id", async (req, res) => {
 });
 
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
