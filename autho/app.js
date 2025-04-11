848c41ff3df0411d8774828a00740aeeab9e1fe0
maanya.g14 <maanya.g14@gmail.com>
2025-04-11 21:44:38 +0530

    ch

--- autho/app.js
+++ autho/app.js
@@ -58,6 +58,10 @@ async function run() {
   }
 }
 run().catch(console.dir);
+require('dotenv').config();
+const port = process.env.PORT || 10000;
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 app.get('/login', (req, res) => {
   res.render("login");
